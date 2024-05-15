1236acfd1066b21a3179a970b097ae083a9ce571
jadenfk@outlook.com <jadenfk@outlook.com>
2024-05-15 14:53:21 -0400

    sidebar

--- vueapp/src/components/Sidebar.vue
+++ vueapp/src/components/Sidebar.vue
@@ -1,21 +1,33 @@
 <template>
-    
+    <Sidebar :visible="sidebar_visible" header="Menu">
+        <Panel header="Interventions" toggleable>
+            <Intervention v-for="intervention in interventions" :name="intervention.name" :fields="intervention.fields">
+            </Intervention>
+        </Panel>
+    </Sidebar>
+    <Button label="Menu" @click="sidebar_visible = true" style="position:sticky;top:20px;left:20px" />
 </template>
 <style></style>
 <script>
 
-
+import Button from 'primevue/button';
+import Panel from 'primevue/panel';
+import Sidebar from 'primevue/sidebar';
+import Intervention from './Intervention.vue';
 
 export default {
     components: {
-  
+        Sidebar,
+        Button,
+        Panel,
+        Intervention
     },
     props: {
-  
+        interventions: Array
     },
     data() {
         return {
-            
+
         }
     },
     methods: {
@@ -23,4 +35,3 @@ export default {
     }
 
 }</script>
-
